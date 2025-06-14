addde8f37521a9cee2361461fb8f295205435267
Roman Zvonyk <jagerluck@gmail.com>
2025-06-14 09:40:58 +0300

    learning read operations

--- main.go
+++ main.go
@@ -16,18 +16,30 @@ func main() {
 
     defer f.Close()
 
-    for {
-        data := make([]byte, 8)
-        n, err := f.Read(data)
-
-        if err != nil {
-            break
-        }
-
-        fmt.Printf("read: %s\n", string(data[:n]))
-    }
+    getLineChannel(f)
 }
 
 func getLineChannel(f io.ReadCloser) <-chan string {
+    out := make(chan string, 1)
 
+    go func() {
+        defer close(out)
+        defer f.Close()
+
+        str := ""
+        for {
+            data := make([]byte, 8)
+            n, err := f.Read(data)
+            if err != nil {
+                break
+            }
+
+            data = data[:n] // to save space?
+
+        }
+    }()
+
+    fmt.Printf("read: %s\n", out)
+
+    return out
 }
